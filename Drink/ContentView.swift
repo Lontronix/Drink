2e7968940b37a2ac6d1d56750aa732f10efcb6f5
Lonnie <Lonniegerol@gmail.com>
2019-09-11 21:39:57 -0400

    added project files

---
+++ Drink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drink
+//
+//  Created by Lonnie Gerol on 9/11/19.
+//  Copyright © 2019 Lonnie Gerol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
